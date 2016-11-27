dbdd0eaa72a19c2ee65e938202c0138d79816d0d
Xueyin Wang <wangsnowyin@Xueyins-MacBook-Pro.local>
2016-11-27 12:36:36 -0600

    inital path added with 3 policies

--- src/edu/uchicago/cs/ucare/samc/server/DfsTreeTravelModelChecker.java
+++ src/edu/uchicago/cs/ucare/samc/server/DfsTreeTravelModelChecker.java
@@ -3,16 +3,25 @@ package edu.uchicago.cs.ucare.samc.server;
 import java.util.LinkedList;
 import java.util.ListIterator;
 
+import edu.uchicago.cs.ucare.samc.event.Event;
+import edu.uchicago.cs.ucare.samc.transition.PacketSendTransition;
 import edu.uchicago.cs.ucare.samc.transition.Transition;
+import edu.uchicago.cs.ucare.samc.util.LocalState;
 import edu.uchicago.cs.ucare.samc.util.WorkloadDriver;
 
 public class DfsTreeTravelModelChecker extends TreeTravelModelChecker {
+	
+	boolean enableNodeTwoEvent;
+    boolean enableNodeTwoSync;
 
     public DfsTreeTravelModelChecker(String interceptorName, String ackName, int numNode,
             int numCrash, int numReboot, String globalStatePathDir, String packetRecordDir,
             String workingDir, WorkloadDriver workloadDriver, String ipcDir) {
         super(interceptorName, ackName, numNode, numCrash, numReboot, globalStatePathDir, 
                 packetRecordDir, workingDir, workloadDriver, ipcDir);
+        
+        this.enableNodeTwoEvent = false;
+        this.enableNodeTwoSync = false;
     }
     
     @Override
@@ -20,6 +29,34 @@ public class DfsTreeTravelModelChecker extends TreeTravelModelChecker {
     	ListIterator<Transition> iter = transitions.listIterator();
         while (iter.hasNext()) {
             Transition transition = iter.next();
+            
+            if(interceptorName.equals("zkChecker-3.5.1")) {
+            	if(transition instanceof PacketSendTransition) {
+            		LocalState ls0 = ((PacketSendTransition) transition).getChecker().localStates[0];
+            		LocalState ls1 = ((PacketSendTransition) transition).getChecker().localStates[1];
+            		Integer state1 = (Integer)ls0.getValue("state");
+            		Integer state2 = (Integer)ls1.getValue("state");
+            		if(state1 != null && state2 != null) {
+            			if((state1==1 && state2==2) || (state1==2 && state2==1))
+            				enableNodeTwoEvent = true;
+            		}
+            		
+            		Event event = ((PacketSendTransition) transition).getPacket();
+                	if(event.getFromId() == 2 && !enableNodeTwoEvent) {
+                		continue;
+                	}
+                	
+                	if(((String)event.getValue(Event.FILENAME)).substring(0, 3).equals("rc-")) {
+                		enableNodeTwoSync = true;
+                	}
+                	
+                	if(((String)event.getValue(Event.FILENAME)).substring(0, 5).equals("sync-") 
+                			&& (event.getFromId()==2 || event.getToId()==2) && !enableNodeTwoSync) {
+                		continue;
+                	}
+                }
+            }
+            
             if (!exploredBranchRecorder.isSubtreeBelowChildFinished(transition.getTransitionId())) {
                 iter.remove();
                 return transition;
--- src/edu/uchicago/cs/ucare/samc/server/RandomModelChecker.java
+++ src/edu/uchicago/cs/ucare/samc/server/RandomModelChecker.java
@@ -13,9 +13,11 @@ import edu.uchicago.cs.ucare.samc.transition.Transition;
 import edu.uchicago.cs.ucare.samc.transition.AbstractNodeOperationTransition;
 import edu.uchicago.cs.ucare.samc.transition.AbstractNodeCrashTransition;
 import edu.uchicago.cs.ucare.samc.transition.AbstractNodeStartTransition;
+import edu.uchicago.cs.ucare.samc.event.Event;
 import edu.uchicago.cs.ucare.samc.raft.AbstractRaftSnapshot;
 import edu.uchicago.cs.ucare.samc.util.WorkloadDriver;
 import edu.uchicago.cs.ucare.samc.util.ExploredBranchRecorder;
+import edu.uchicago.cs.ucare.samc.util.LocalState;
 import edu.uchicago.cs.ucare.samc.util.SqliteExploredBranchRecorder;
 
 public class RandomModelChecker extends ModelCheckingServerAbstract {
@@ -28,6 +30,9 @@ public class RandomModelChecker extends ModelCheckingServerAbstract {
     String stateDir;
     Random random;
     
+    boolean enableNodeTwoEvent;
+    boolean enableNodeTwoSync;
+    
     public RandomModelChecker(String inceptorName, String ackName, int maxId,
             int numCrash, int numReboot, String globalStatePathDir, String packetRecordDir, String workingDir,
             WorkloadDriver workloadDriver, String ipcDir) {
@@ -35,6 +40,10 @@ public class RandomModelChecker extends ModelCheckingServerAbstract {
         this.numCrash = numCrash;
         this.numReboot = numReboot;
         stateDir = packetRecordDir;
+        
+        this.enableNodeTwoEvent = false;
+        this.enableNodeTwoSync = false;
+        
         try {
             exploredBranchRecorder = new SqliteExploredBranchRecorder(packetRecordDir);
         } catch (SQLiteException e) {
@@ -68,8 +77,39 @@ public class RandomModelChecker extends ModelCheckingServerAbstract {
     	LinkedList<Transition> cloneQueue = (LinkedList<Transition>) transitions.clone();
         while (cloneQueue.size()>0) {
         	int i = random.nextInt(cloneQueue.size());
-        	Transition cloneTransition = cloneQueue.remove(i);
-            if(!exploredBranchRecorder.isSubtreeBelowChildFinished(cloneTransition.getTransitionId())) {
+        	Transition cloneTransition;
+        	if(interceptorName.equals("zkChecker-3.5.1")) {
+        		cloneTransition = cloneQueue.get(i);
+            	if(cloneTransition instanceof PacketSendTransition) {
+            		LocalState ls0 = ((PacketSendTransition) cloneTransition).getChecker().localStates[0];
+            		LocalState ls1 = ((PacketSendTransition) cloneTransition).getChecker().localStates[1];
+            		Integer state1 = (Integer)ls0.getValue("state");
+            		Integer state2 = (Integer)ls1.getValue("state");
+            		if(state1 != null && state2 != null) {
+            			if((state1==1 && state2==2) || (state1==2 && state2==1))
+            				enableNodeTwoEvent = true;
+            		}
+            		
+            		Event event = ((PacketSendTransition) cloneTransition).getPacket();
+                	if(event.getFromId() == 2 && !enableNodeTwoEvent) {
+                		continue;
+                	}
+                	
+                	if(((String)event.getValue(Event.FILENAME)).substring(0, 3).equals("rc-")) {
+                		enableNodeTwoSync = true;
+                	}
+                	
+                	if(((String)event.getValue(Event.FILENAME)).substring(0, 5).equals("sync-") 
+                			&& (event.getFromId()==2 || event.getToId()==2) && !enableNodeTwoSync) {
+                		continue;
+                	}
+                	cloneQueue.remove(i);
+                }
+            } else {
+            	cloneTransition = cloneQueue.remove(i);
+            } 	
+        	
+        	if(!exploredBranchRecorder.isSubtreeBelowChildFinished(cloneTransition.getTransitionId())) {
             	for(int j=0; j<transitions.size(); j++){
             		Transition transition = transitions.get(j);
             		if(cloneTransition.getTransitionId() == transition.getTransitionId()){
